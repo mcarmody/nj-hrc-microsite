61e328d2d663de98b81fcf8f5f2318b4b76df299
Mike <mcarmody2013@gmail.com>
2021-11-08 21:01:38 -0500

    made button trigger dynamic with state change, placeholder copy
    need to make delays work for copy

--- src/components/question-overlay.js
+++ src/components/question-overlay.js
@@ -34,7 +34,9 @@ class QuestionOverlay extends React.Component {
 		this.state = {
 			stage: 1,
 			video: mountainsVid,
-			copy: "<span style={headingBoldStyles}>Here in New Jersey,</span><br />when it comes to<br />opioids and overdoses..."
+			copy: "<span style={headingBoldStyles}>Here in New Jersey,</span><br />when it comes to<br />opioids and overdoses...",
+			yesVal: 2,
+			noVal: 3
 		};
 	}
 
@@ -54,26 +56,38 @@ class QuestionOverlay extends React.Component {
 				console.log(this.state.video)
 				break;
 			case 2: //this is the second video
-				this.setState({video: rainVid, copy: "Wrong! New video!"})
+				this.setState({video: rainVid, copy: "Wrong! New video!", yesVal: 4, noVal: 5})
 				console.log(this.state.video)
 				break;
 			case 3: //this is the second question page
-				this.setState({video: godraysVid, copy: "Good answer. New question!"})
+				this.setState({video: godraysVid, copy: "Good answer. New question!", yesVal: 4, noVal: 5})
 				console.log(this.state.video)
-				break
+				break;
+			case 4: //this is the third video
+				this.setState({video: mountainsVid, copy: "Wrong! New video!", yesVal: 6, noVal: 6})
+				console.log(this.state.video)
+				break;
+			case 5: //this is the third question page
+				this.setState({video: rainVid, copy: "Good answer. New question!", yesVal: 6, noVal: 6})
+				console.log(this.state.video)
+				break;
+			case 6: //this is the last video
+				this.setState({video: godraysVid, copy: "You're done!"})
+				console.log(this.state.video)
+				break;
 		}
 		
 	}
 
 	componentDidMount() {
 		this.updateCopy();
-		setTimeout(function() {
+		setTimeout(function() { //the first round of copy appearing
 			document.getElementsByClassName("video-overlay-copy")[0].classList.remove("hidden");
 			document.getElementsByClassName("overlay-background")[0].classList.remove("hidden");
 			document.getElementById("landing-clip").classList.add("hidden");
 		}, firstVideoDuration);
 
-		setTimeout(function() {
+		setTimeout(function() { //second part of initial question
 			document.getElementsByClassName("video-overlay-copy")[0].innerHTML = secondTextUpdate;
 			document.getElementsByClassName("overlay-buttons-container")[0].classList.remove("hidden");
 		}, firstVideoDuration + betweenCopyDelay);
@@ -97,8 +111,8 @@ class QuestionOverlay extends React.Component {
 					<h1 className = "video-overlay-copy hidden">
 					</h1>
 					<div className = "overlay-buttons-container hidden">
-						<div className = "yes-button" onClick={() => this.nextQuestion(2)}>Yes</div>
-						<div className = "no-button" onClick={() => this.nextQuestion(3)}>No</div>
+						<div className = "yes-button" onClick={() => this.nextQuestion(this.state.yesVal)}>Yes</div>
+						<div className = "no-button" onClick={() => this.nextQuestion(this.state.noVal)}>No</div>
 					</div>
 				</div>
 			</div>
