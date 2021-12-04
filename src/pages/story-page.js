9df19bd4697508dfe6f73346d1a86e91b750e2a4
Mike <mcarmody2013@gmail.com>
2021-12-04 16:04:18 -0500

    add gradient for larger displays

--- src/pages/story-page.js
+++ src/pages/story-page.js
@@ -13,8 +13,8 @@ const rawContent = [
         //How We Got Here
         {type: "link", data: "/img/HOW+WE+GOT+HERE+STAT.JPG", title: "Cost of the Drug War", metadata: '<span className = "stat-highlight">$11.6 billion</span>spent on drug war arrests <span className = "stat-highlight">544.6 times</span>more than investment in harm reduction services'},
         {type: "link", data: "/img/HOW+WE+GOT+HERE+TESTIMONIAL.JPG", title: "Testimonial", metadata: "“New Jersey law states you need local municipal ordinance to have a syringe access program. A lot of municipal leaders don’t have a public health background and don’t understand that syringe access saves lives, saves money, and can help clean up the city.”", metadata2: "—Georgett Watson, South Jersey AIDS Alliance"},
-        {type: "image", data: "/img/harm_reduction_test.png", title: "Share the campaign", metadata: ""},
-        {type: "video", data: "/vid/godrays-test.mp4", title: "Rates of HIV where you live", metadata: "", metadata2: "/img/harm_reduction_test.png"},
+        {type: "video", data: "/vid/godrays-test.mp4", title: "Share the campaign", metadata: "", metadata2: "/img/how+we+got+here.jpg"},
+        {type: "image", data: "/img/how-we-got-here_icon.png", title: "Rates of HIV where you live", metadata: "", metadata2: "/img/harm_reduction_test.png"},
         //second group
         {type: "link", data: "http://www.mikecarmody2.net", title: "", metadata: "a test link"},
         {type: "link", data: "http://www.mikecarmody2.net", title: "", metadata: "another test link"},
@@ -113,7 +113,6 @@ class IndividualStory extends React.Component {
             <div className = "story-title">{this.state.title}</div>
             <div className = "story-content" style = {{backgroundImage: "url("+mediaLinkUrl+this.state.selectedItem.data+")"}}>
               <div className = "story-copy" dangerouslySetInnerHTML={{ __html: this.state.description}} />
-              
               {this.state.selectedItem.type=="video" ? (
                 <video autoPlay="autoplay" muted loop id="story-clip" className = "story-video-container">
                   <source src = {mediaLinkUrl+this.state.selectedItem.data} type="video/mp4" className = "story-video"/>
@@ -124,12 +123,11 @@ class IndividualStory extends React.Component {
           </div>
           <div className = "side-nav-container">
             <Link to="/stories-hub" className = "back-link">Home</Link>           
-            < SideNavItem type = {rawContent[(this.state.id*4)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4)].data} title = {rawContent[(this.state.id*4)].title} onClick={() => this.selectItem((this.state.id*4))} />
-            < SideNavItem type = {rawContent[(this.state.id*4+1)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4+1)].data} title = {rawContent[(this.state.id*4+1)].title} onClick={() => this.selectItem((this.state.id*4+1))} />
-            < SideNavItem type = {rawContent[(this.state.id*4+2)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4+2)].data} title = {rawContent[(this.state.id*4+2)].title} onClick={() => this.selectItem((this.state.id*4+2))} />
+            < SideNavItem type = {rawContent[(this.state.id*4)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4)].data} title = {rawContent[(this.state.id*4)].title} metadata2 = {mediaLinkUrl+rawContent[(this.state.id*4+3)].metadata2} onClick={() => this.selectItem((this.state.id*4))} />
+            < SideNavItem type = {rawContent[(this.state.id*4+1)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4+1)].data} title = {rawContent[(this.state.id*4+1)].title} metadata2 = {mediaLinkUrl+rawContent[(this.state.id*4+1)].metadata2} onClick={() => this.selectItem((this.state.id*4+1))} />
+            < SideNavItem type = {rawContent[(this.state.id*4+2)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4+2)].data} title = {rawContent[(this.state.id*4+2)].title} metadata2 = {mediaLinkUrl+rawContent[(this.state.id*4+2)].metadata2} onClick={() => this.selectItem((this.state.id*4+2))} />
             < SideNavItem type = {rawContent[(this.state.id*4+3)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4+3)].data} title = {rawContent[(this.state.id*4+3)].title} metadata2 = {mediaLinkUrl+rawContent[(this.state.id*4+3)].metadata2} onClick={() => this.selectItem((this.state.id*4+3))} />
-            < SideNavItem type = {rawContent[(this.state.id*4+3)].type} data = {mediaLinkUrl+rawContent[(this.state.id*4+3)].data} title = {rawContent[(this.state.id*4+3)].title} metadata2 = {mediaLinkUrl+rawContent[(this.state.id*4+3)].metadata2} onClick={() => this.selectItem((this.state.id*4+3))} />
-          
+           
             < ActionLinks social = {true} donate = {true}/>
           </div>
           < Tagline size = "small" position = "extra-low"/>
