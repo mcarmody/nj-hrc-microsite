30d47e24a2e9432895f7412ca10aa9a15c812e50
Joseph D. Sumner <me@jds.dev>
2021-12-07 19:44:22 -0600

    Remove NJHRC logo from stories hub

--- src/pages/stories-hub.js
+++ src/pages/stories-hub.js
@@ -62,7 +62,6 @@ class StoriesHub extends React.Component {
         < NavMovie source= {imageList[3]} id = "3" videoName = "fourth" title = {titlesList[3]}/>
         < NavMovie source= {imageList[4]} id = "4" videoName = "fifth" title = {titlesList[4]}/>
         < Tagline size = "small" />
-        < Logo wordmark = {true} sideCopy = {false}/>
         <div className = "mobile-modal">This site works best if you hold your device in landscape orientation.</div>
       </main>
     )
